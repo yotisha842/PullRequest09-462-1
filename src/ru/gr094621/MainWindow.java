1d1db79cc9d0f40680f09c02ba6231c8b38510a4
Smak2025 <smak-80+2025@yandex.ru>
2026-04-13 09:11:18 +0300

    13.04

--- src/ru/gr094621/MainWindow.java
+++ src/ru/gr094621/MainWindow.java
@@ -1,4 +1,10 @@
 package ru.gr094621;
 
+import javax.swing.*;
+
 public class MainWindow {
+    private JPanel graphPanel;
+    public MainWindow(){
+
+    }
 }
--- src/ru/gr094621/painting/Painter.java
+++ src/ru/gr094621/painting/Painter.java
@@ -1,4 +1,9 @@
 package ru.gr094621.painting;
 
+import java.awt.*;
+
 public interface Painter {
+    int getWidth();
+    int getHeight();
+    void paint(Graphics g);
 }
